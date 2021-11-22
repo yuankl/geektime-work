9440ee8603b841113a79bcac11344c1375e4be00
andy <274219962@qq.com>
2021-11-22 18:19:10 +0800

    初始化yeoman

--- week11/generators/app/index.js
+++ week11/generators/app/index.js
@@ -1,14 +1,44 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-    constructor(args, opts){
+    constructor(args, opts) {
         super(args, opts);
         this.option('babel');
     }
-    method1(){
-        this.log("method 1 just ran");
+    initPackage(){
+        const pkgJson = {
+            devDependencies: {
+                eslint: '^3.15.0'
+            },
+            dependencies: {
+                react: '^16.2.0'
+            }
+        };
+
+        // Extend or create package.json file in destination path
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+        // this.npmInstall();
     }
-    method2(){
-        this.log("method 2 just ran");
+    async method1() {
+        // const answers = await this.prompt([
+        //     {
+        //         type: "input",
+        //         name: "name",
+        //         message: "You project name",
+        //         default: this.appname// default to current folder name
+        //     },
+        //     {
+        //         type: "confirm",
+        //         name: "cool",
+        //         message: "Would you like to enable the Cool feature?"
+        //     }
+        // ]);
+        // this.log("app name", answers.name);
+        // this.log("cool feature",  answers.cool)
+        this.fs.copyTpl(
+            this.templatePath('t.html'),
+            this.destinationPath('public/index.html'),
+            { title: 'Templating with Yeoman' }
+        );
     }
 };
